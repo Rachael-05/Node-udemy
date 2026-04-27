a8c021c2453392e85be75203dcedde9af51e7654
Rachael-05 <rachhall05@gmail.com>
2026-04-27 12:46:52 -0400

    4/27

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const http =require('http');
+const http = require('http');
 const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
@@ -17,64 +17,75 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 // Non-blocking, asynchronous way
 //fs.readFile('.txt/start.txt', 'utf-8', (err, data1) => {
-  //if (err) return console.log('ERROR!');
-  //fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-  //console.log(data2);
-   //fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-   //console.log(data3);
+//if (err) return console.log('ERROR!');
+//fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//console.log(data2);
+//fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//console.log(data3);
 
-   //fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-   //  console.log('your file has been written :3')
-   //})
-  // });
- // });
- //});
+//fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//  console.log('your file has been written :3')
+//})
+// });
+// });
+//});
 //console.log('Will read file!');
 
 /////////////////////////////////////////
 // SERVER
-    const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-    const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-    const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8',
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8',
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8',
+);
 
-    const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-    const dataObj =  JSON.parse(data);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
-  const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
   //Overview page
-  if(pathName === '/' || pathName === '/overview'){
-    res.writeHead(200, { 'Content-type': 'text/html'});
+  if (pathName === '/' || pathName === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
 
     res.end('tempOverview');
 
-  //Product page
-  }   else if (pathName === '/product') {
-    res.writeHead(200, { 'Content-type': 'text/html'});
+    //Product page
+  } else if (pathName === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
-  //API
-  }   else if (pathName === '/api') {
-    res.writeHead(200, { 'Content-type': 'application/json'});
+    //API
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
-  //Not found
-  }   else {
+    //Not found
+  } else {
     res.writeHead(404, {
       'content-type': 'text/html',
-      'my-own-header': 'hello-world'
+      'my-own-header': 'hello-world',
     });
-    res.end('<h1>Page not found!</h1>'); 
+    res.end('<h1>Page not found!</h1>');
   }
 });
 
